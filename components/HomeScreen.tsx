57ddff36d56a3fc3c4c1de4f31da7a4881f7d0c7
Arthur Lee <arthur@arthurlee.me>
2020-03-30 19:20:35 -0700

    Add fade-in animation

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,5 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { SafeAreaView, ScrollView, StyleSheet, View, Text } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Animated,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+} from "react-native";
 import * as Location from "expo-location";
 
 import LastUpdatedLabel from "./LastUpdatedLabel";
@@ -46,6 +53,14 @@ export default function HomeScreen({ navigation }) {
     }
   }
 
+  const fadeInAnim = useRef(new Animated.Value(0)).current;
+  const fadeInWeatherBlock = () => {
+    Animated.timing(fadeInAnim, {
+      toValue: 1,
+      duration: 500,
+    }).start();
+  };
+
   useEffect(() => {
     if (!location) {
       fetchLocation();
@@ -54,6 +69,7 @@ export default function HomeScreen({ navigation }) {
       setWeatherData({ ...weatherData, status: "pending" });
       fetchWeather(location).then(newWeatherData => {
         setWeatherData(newWeatherData);
+        fadeInWeatherBlock();
       });
     }
   }, [location]);
@@ -64,13 +80,23 @@ export default function HomeScreen({ navigation }) {
         <View style={styles.titleContainer}>
           <Text style={styles.titleText}>{location.placeName}</Text>
         </View>
-        {weatherData.status === "success" ? (
-          <CurrentWeatherBlock
-            temperature={weatherData.now.apparentTemperature}
-          />
-        ) : weatherData.status === "pending" ? (
+        <View style={styles.relativeContainer}>
           <LoadingWeatherBlock />
-        ) : null}
+          <Animated.View
+            style={[
+              styles.weatherBlockContainer,
+              {
+                opacity: fadeInAnim,
+              },
+            ]}
+          >
+            {weatherData.status === "success" ? (
+              <CurrentWeatherBlock
+                temperature={weatherData.now.apparentTemperature}
+              />
+            ) : null}
+          </Animated.View>
+        </View>
         <View style={styles.lastUpdatedContainer}>
           <LastUpdatedLabel date={weatherData.lastUpdated} />
         </View>
@@ -128,5 +154,14 @@ const styles = StyleSheet.create({
     fontSize: 45,
     letterSpacing: -0.5,
     fontWeight: "100",
+  },
+  relativeContainer: {
+    position: "relative",
+  },
+  weatherBlockContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
   },
 });
